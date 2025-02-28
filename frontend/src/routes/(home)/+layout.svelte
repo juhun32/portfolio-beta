913f984610ccbf6c1185971461a47a8cca6438bc
Juhun <riann3207@gmail.com>
2025-02-28 01:20:05 -0500

    feat(frontend) enhance home page with dynamic text and profile image; update layout for better structure

--- frontend/src/routes/(home)/+layout.svelte
+++ frontend/src/routes/(home)/+layout.svelte
@@ -11,30 +11,35 @@
 
     import { Button } from "$lib/components/ui/button/index.js";
     import Combobox from "$lib/components/Combobox/Combobox.svelte";
+
+    import Avatar from "$lib/components/Avatar/Avatar.svelte";
 </script>
 
 <ModeWatcher />
 <div class="min-h-screen">
-    <header class="bg-background z-50 flex justify-center">
+    <header class="bg-background sticky top-0 z-50 flex justify-center">
         <div
-            class="fixed w-full mx-auto flex items-center justify-center lg:px-6 border-b border-dashed border-stone-300 dark:border-stone-800"
+            class="w-full mx-auto flex items-center justify-center border-b border-dashed border-stone-300 dark:border-stone-800"
         >
             <div
-                class="w-3/4 sm:w-4/5 backdrop-blur-md py-2 flex items-center justify-between"
+                class="w-3/4 sm:w-4/5 backdrop-blur-md py-2 flex items-center justify-between border-x border-dashed border-stone-300 dark:border-stone-800"
             >
                 <a
                     href="/home"
                     aria-label="Home"
-                    class="group hover:opacity-80 transition-all"
+                    class="group flex items-center hover:opacity-80 transition-all"
                 >
+                    <div class="ml-8 rounded-lg border border-stone-950 overflow-hidden dark:bg-white">
+                        <Avatar className="h-7" />
+                    </div>
                     <p
-                        class="ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-10 px-4 py-2 text-xs sm:text-sm"
+                        class="ring-offset-background focus-visible:ring-ring inline-flex items-center justify-center whitespace-nowrap rounded-md font-medium transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 hover:text-accent-foreground h-10 px-3 py-2 text-md sm:text-lg"
                     >
-                        svg logo
+                        Juhun Park
                     </p>
                 </a>
                 <div class="flex">
-                    <div class="flex items-center gap-2">
+                    <div class="flex items-center gap-2 mr-3">
                         <!-- <Combobox /> -->
                         <Button
                             on:click={toggleMode}
@@ -58,7 +63,7 @@
         <div
             class="w-3/4 sm:w-4/5 p-6 border-x border-dashed border-stone-300 dark:border-stone-800"
         >
-            <p class="text-sm text-stone-500">Built by Juhun Park</p>
+            <p class="text-sm text-stone-500">Built by Juhun Park <a href="https://github.com/juhun32/portfolio" class="hover:text-pink-500/50 hover:underline decoration-pink-500/50">[GitHub]</a></p>
         </div>
     </footer>
 </div>
--- frontend/src/routes/(home)/+page.svelte
+++ frontend/src/routes/(home)/+page.svelte
@@ -1,15 +1,61 @@
 <script lang="ts">
     import Projects from "../(app)/projects/+page@(app).svelte";
+    import Profile from "./images/profile.png";
+
+    // changing text
+    import { onMount, onDestroy } from "svelte";
+    import { fade } from "svelte/transition";
+
+    let texts = ["Software Engineer", "Racing Enthusiast", "College Student"];
+    let index = 0;
+    let currentText = texts[index];
+
+    let interval: number;
+
+    onMount(() => {
+        interval = setInterval(() => {
+            index = (index + 1) % texts.length;
+            currentText = texts[index];
+        }, 5000); // Change text every 2 seconds
+    });
+
+    onDestroy(() => {
+        clearInterval(interval);
+    });
 </script>
 
-<div class="h-screen flex flex-col justify-center gap-4 items-center h-full">
-    <div class="h-full w-3/4 sm:w-4/5 flex flex-col justify-center gap-2 items-center border-x border-dashed border-stone-300 dark:border-stone-800">
-        <h1 class="text-2xl sm:text-4xl font-bold text-center ">
-            Juhun Park <span class="text-violet-500">Software Engineer</span>
-        </h1>
-        <p class="text-base sm:text-xl text-center w-3/4 sm:w-1/2">
-            Description
-        </p>
+<div class="flex flex-col justify-center gap-4 items-center">
+    <div
+        class="w-3/4 sm:w-4/5 p-8 grid grid-cols-[1fr_2fr] items-center py-8 md:py-10 lg:py-12 border-x border-dashed border-stone-300 dark:border-stone-800"
+    >
+        <div class="flex justify-center items-center gap-1">
+            <p class="text-xs text-stone-400" style="writing-mode: vertical-rl;">
+                my beautiful girlfriend's favorite picture of me :)
+            </p>
+            <img
+                src={Profile}
+                alt="Profile"
+                class="h-[280px] w-[210px] aspect-[3/4] object-cover p-1 border rounded-xl overflow-hidden dark:bg-white"
+            />
+        </div>
+        <div class="flex flex-col items-start pr-8 gap-3">
+            <h1
+                class="flex items-baseline gap-2 text-2xl sm:text-4xl font-bold"
+            >
+                Juhun Park <p class="text-sm">박주훈</p>
+            </h1>
+            {#key currentText}
+                <span
+                    class="text-2xl sm:text-4xl font-bold text-yellow-900/80 dark:text-yellow-700/80"
+                    in:fade={{ duration: 500 }}>{currentText}</span
+                >{/key}
+            <p class="text-base sm:text-xl">
+                Hello! I am currently a student of George Mason University,
+                studying computer science and artificial intelligence. I am
+                interested in machine learning application in healthcare
+                industry.
+            </p>
+        </div>
     </div>
 </div>
 
