7a9665572c15d609620830abf42a400267c05c7c
Juhun Park <riann3207@gmail.com>
2025-05-21 19:36:02 -0400

    feat(frontend): add static adapter and update dependencies for github pages deploy

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -26,6 +26,7 @@
     import { onMount, onDestroy } from "svelte";
     import { fade } from "svelte/transition";
     import Linkedin from "$lib/components/svg/Linkedin/Linkedin.svelte";
+    import { base } from "$app/paths";
 
     // changing text for the main page
     let texts = [
@@ -60,7 +61,7 @@
                 class="w-3/4 sm:w-4/5 backdrop-blur-md py-2 flex items-center justify-between border-x border-dashed border-stone-300 dark:border-stone-800"
             >
                 <a
-                    href="/"
+                    href="{base}/"
                     aria-label="Home"
                     class="group flex items-center hover:opacity-80 transition-all"
                 >
