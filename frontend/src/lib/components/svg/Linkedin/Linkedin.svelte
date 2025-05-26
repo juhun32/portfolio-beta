de88ea08c81e7dc120840df9d8120c8a87f5918a
Juhun Park <riann3207@gmail.com>
2025-05-26 17:37:02 -0400

    feat: testing bugged out linkedin icon

--- frontend/src/lib/components/svg/Linkedin/Linkedin.svelte
+++ frontend/src/lib/components/svg/Linkedin/Linkedin.svelte
@@ -2,7 +2,14 @@
     export let className: string;
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" class={className} viewBox="0 0 72 72" fill="none" role="img" aria-labelledby="linkedin-title">
+<!-- <svg
+    xmlns="http://www.w3.org/2000/svg"
+    class={className}
+    viewBox="0 0 72 72"
+    fill="none"
+    role="img"
+    aria-labelledby="linkedin-title"
+>
     <path
         d="M8,72 L64,72 C68.418278,72 72,68.418278 72,64 L72,8 C72,3.581722 68.418278,-8.11624501e-16 64,0 L8,0 C3.581722,8.11624501e-16 -5.41083001e-16,3.581722 0,8 L0,64 C5.41083001e-16,68.418278 3.581722,72 8,72 Z"
         fill="currentColor"
@@ -11,4 +18,16 @@
         fill="currentColor"
         style="filter: invert(100%);"
     /></svg
+> -->
+
+<svg
+    role="img"
+    viewBox="0 0 48 48"
+    xmlns="http://www.w3.org/2000/svg"
+    class={className}
+    fill="currentColor"
 >
+    <path
+        d="M41,4H9C6.24,4,4,6.24,4,9v32c0,2.76,2.24,5,5,5h32c2.76,0,5-2.24,5-5V9C46,6.24,43.76,4,41,4z M17,20v19h-6V20H17z M11,14.47c0-1.4,1.2-2.47,3-2.47s2.93,1.07,3,2.47c0,1.4-1.12,2.53-3,2.53C12.2,17,11,15.87,11,14.47z M39,39h-6c0,0,0-9.26,0-10 c0-2-1-4-3.5-4.04h-0.08C27,24.96,26,27.02,26,29c0,0.91,0,10,0,10h-6V20h6v2.56c0,0,1.93-2.56,5.81-2.56 c3.97,0,7.19,2.73,7.19,8.26V39z"
+    ></path>
+</svg>
